d7b33235d43f3967f8671262170dc5944e21a811
Arpit-Singh22 <arpitsingh22@hotmail.com>
2025-11-26 11:55:13 +0000

    created list.svh and run.do

--- sim/list.svh
+++
@@ -1,3 +0,0 @@
-`include "../wb_intf/wb_intf.sv"
-`include "../rtl/uart_top.v"
-`include "../top/top.sv"
--- sim/questa_uvm.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-call "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
-
-set QUESTAPATH=C:\questasim64_2024.1\win64
-set UVM_DPI_PATH=C:\questasim64_2024.1\uvm-1.2\win64
-
-"C:\questasim64_2024.1\win64\vsim.exe" -sv_lib "C:\questasim64_2024.1\uvm-1.2\win64\uvm_dpi"
